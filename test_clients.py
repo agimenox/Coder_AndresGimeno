638eacdec50b2dff939ea41c0268816b3dac6c07
Andres Gimeno <agimenox@gmail.com>
2023-05-29 21:15:06 +0100

    fixed test case

--- test_clients.py
+++ test_clients.py
@@ -6,7 +6,7 @@ def test_assign_num():
     assert a_client.number_set(54321) == 54321
 
 def test_purchase():
-    assert a_client.purchase('Cocacola',500,3) == 1500
+    assert a_client.purchase('Cocacola',500,3) == '1500 units of Cocacola'
     
 def test_print_object():
     assert str(a_client).startswith('This object is relate to:')
